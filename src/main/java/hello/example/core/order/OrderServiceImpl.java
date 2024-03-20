2db755c6cbc3847aca9ec0418add30c7868ce7f3
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-03-20 22:14:25 +0900

    03120_DIP

--- src/main/java/hello/example/core/order/OrderServiceImpl.java
+++ src/main/java/hello/example/core/order/OrderServiceImpl.java
@@ -10,7 +10,11 @@ import hello.example.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository=new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //현재 구체적인것에도 의존, 추상적인 것에도 의존, DIP위반
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //할인 정책을 바꾸는데 OrderServiceImpl을 수정하게 됨
+    //그래서 이렇게 수정함
+    private DiscountPolicy discountPolicy;
 
     //주문 생성 요청이 오면, 회원 조회를 하고, 할인 정책에 회원을 넘겨줌
     //
